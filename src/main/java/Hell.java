4097704821401abd2e87244faf4e41c7110f678d
Zandbee <vesna.adrs@gmail.com>
2016-05-17 09:49:40 +0300

    create out zip with txt

--- src/main/java/Hell.java
+++ src/main/java/Hell.java
@@ -1,9 +1,7 @@
 import java.io.*;
-import java.util.Enumeration;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipException;
-import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 /**
  * Created by vstrokova on 12.05.2016.
@@ -13,10 +11,13 @@ public class Hell {
     public static void main(String args[]) throws Exception {
         ZipInputStream zis = new ZipInputStream(new BufferedInputStream(new FileInputStream("inputs.zip")));
 
-        extract(zis);
+        copyZip(zis);
     }
 
-    private static void extract(ZipInputStream zipInputStream) throws Exception {
+    private static void copyZip(ZipInputStream zipInputStream) throws Exception {
+        FileOutputStream dest = new FileOutputStream("inputsv2.zip");
+        ZipOutputStream out = new ZipOutputStream(new BufferedOutputStream(dest));
+
         while (zipInputStream.available() > 0) {
             ZipEntry zipEntry = zipInputStream.getNextEntry();
             if (zipEntry == null) break;
@@ -26,13 +27,22 @@ public class Hell {
 
             if (entryName.endsWith(".zip")) {
                 ZipInputStream zis = new ZipInputStream(zipInputStream);
-                extract(zis);
+                copyZip(zis);
             } else if (entryName.endsWith(".txt")) {
-                int c;
-                while ((c = zipInputStream.read()) != -1) {
-                    System.out.print((char) c);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(zipInputStream));
+                StringBuilder sb = new StringBuilder();
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line).append("\r\n");
                 }
+
+                ZipEntry newZipEntry = new ZipEntry(entryName);
+                out.putNextEntry(newZipEntry);
+                out.write(sb.toString().getBytes("UTF-8"));
             }
         }
+
+        out.close();
     }
 }
