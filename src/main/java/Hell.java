6ad539b4aed1c974676834d778bdf8a537c11c3c
Veronika Strokova <veronika.strokova@dataart.com>
2016-05-17 17:30:08 +0300

    write files and folders. get phone number from text files

--- src/main/java/Hell.java
+++ src/main/java/Hell.java
@@ -11,38 +11,64 @@ public class Hell {
     public static void main(String args[]) throws Exception {
         ZipInputStream zis = new ZipInputStream(new BufferedInputStream(new FileInputStream("inputs.zip")));
 
-        copyZip(zis);
-    }
-
-    private static void copyZip(ZipInputStream zipInputStream) throws Exception {
         FileOutputStream dest = new FileOutputStream("inputsv2.zip");
         ZipOutputStream out = new ZipOutputStream(new BufferedOutputStream(dest));
 
-        while (zipInputStream.available() > 0) {
-            ZipEntry zipEntry = zipInputStream.getNextEntry();
-            if (zipEntry == null) break;
+        copyZip(zis, out);
 
+        out.close();
+    }
+
+    private static void copyZip(ZipInputStream zipInputStream, ZipOutputStream out) throws Exception {
+        ZipEntry zipEntry;
+        StringBuilder sbPhones, sbEmails;
+        sbPhones = sbEmails = new StringBuilder();
+        int count, pos;
+        char separators[] = {'\t', ' ', ',', ';'};
+        while ((zipEntry = zipInputStream.getNextEntry()) != null) {
             String entryName = zipEntry.getName();
             System.out.println(entryName);
 
             if (entryName.endsWith(".zip")) {
+                System.out.println("Add zip to zip: " + entryName);
                 ZipInputStream zis = new ZipInputStream(zipInputStream);
-                copyZip(zis);
+                out.putNextEntry(new ZipEntry(entryName));
+                /*while ((count = zis.read()) != -1) {
+                    System.out.println("Writing zip");
+                    out.write(count);
+                }*/
+
+                ByteArrayOutputStream byteOutputStream = new ByteArrayOutputStream();
+                ZipOutputStream zos = new ZipOutputStream(byteOutputStream);
+                //byteOutputStream.toByteArray()
+
+                copyZip(zis, zos);
+
             } else if (entryName.endsWith(".txt")) {
+                System.out.println("Add txt to zip: " + entryName);
+
                 BufferedReader reader = new BufferedReader(new InputStreamReader(zipInputStream));
-                StringBuilder sb = new StringBuilder();
+                StringBuilder sbTxt = new StringBuilder();
 
                 String line;
                 while ((line = reader.readLine()) != null) {
-                    sb.append(line).append("\r\n");
+                    if (line.contains("@")) {
+                        pos = line.indexOf("@");
+                        System.out.println("PHONE: " + line.substring(0, line.lastIndexOf(" ", pos)).trim().replace("-", ""));
+                    }
+
+                    sbTxt.append(line).append("\r\n");
                 }
 
                 ZipEntry newZipEntry = new ZipEntry(entryName);
                 out.putNextEntry(newZipEntry);
-                out.write(sb.toString().getBytes("UTF-8"));
-            }
+                out.write(sbTxt.toString().getBytes("UTF-8"));
+            } /*else if (zipEntry.isDirectory()) {
+                System.out.println("Add dir to zip: " + entryName);
+                System.out.println();
+                out.putNextEntry(new ZipEntry(entryName));
+            }*/
         }
-
-        out.close();
+        System.out.println("COPIED");
     }
 }
